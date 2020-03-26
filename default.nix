82b5f237c55150d3c942bf220c8ee7f601f481fc
Joshua Fern <JoshuaFern@ProtonMail.com>
2020-03-25 22:43:07 -0700

    samrewritten

--- default.nix
+++ default.nix
@@ -14,12 +14,11 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  steamgrid = pkgs.callPackage ./pkgs/applications/graphics/steamgrid { };
   dosbox-staging = pkgs.callPackage ./pkgs/misc/emulators/dosbox-staging { };
   #lobase = pkgs.callPackage ./pkgs/tools/misc/lobase { };
   qdl = pkgs.callPackage ./pkgs/development/mobile/qdl { };
-  sbase = pkgs.callPackage ./pkgs/tools/misc/sbase { };
   ubase = pkgs.callPackage ./pkgs/tools/misc/ubase { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  samrewritten = pkgs.callPackage ./pkgs/tools/misc/samrewritten { };
+  steamgrid = pkgs.callPackage ./pkgs/applications/graphics/steamgrid { };
+  ubase = pkgs.callPackage ./pkgs/tools/misc/ubase { };
 }
